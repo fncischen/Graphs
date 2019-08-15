e11db9fa386287e6ed45df174b337551c468665b
Francis Chen <fncis.chen@gmail.com>
2019-08-15 11:22:12 -0700

    completed ancestor problems

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,12 +1,9 @@
 from util import Stack, Queue  # These may come in handy
 
-class SocialGraph:
-    def __init__(self):
-        self.lastID = 0
-        self.ancestors = {}
 
-    def earliest_ancestor(self, ancestors, starting_node):
+def earliest_ancestor(ancestorsData, starting_node):
 
+    ancestors = {}
         # step 1)
 
         # organize the dataset into a graph
@@ -17,70 +14,71 @@ class SocialGraph:
         # to prevent the earliest ancestor from being a cousin
         # by virtue of having lots of uncles and aunts
 
-        for parentChild in ancestors:
-            parent = parentChild[0]
-            child = parentChild[1]
+    for parentChild in ancestorsData:
+        parent = parentChild[0]
+        child = parentChild[1]
 
             # parent has more than one child
-            if parent in self.ancestors.keys():
-                self.ancestors[parent].append(child)
-            else:
-                self.ancestors[parent] = [child] 
+        if parent in ancestors.keys():
+            ancestors[parent].append(child)
+        else:
+            ancestors[parent] = [child] 
 
         # step 2)
 
         # store allPossiblePaths in a cache (array) 
-        allPossiblePaths = []
+    allPossiblePaths = []
 
         # longest path means use depth first search to find the 
         # earliest ancestor
         # set up the algorithim to allow for accessing parents
 
         # use a while loop that stops once the queue is zero
-        s = Stack()
-        s.push([starting_node])
+    s = Stack()
+    s.push([starting_node])
 
-        visited = []
+    visited = []
          # path = []
  
          # make sure current vertex does not equal destination vertex
-        while s.size() > 0: 
-            path = s.pop()
-            currentChild = path[-1]
+    while s.size() > 0: 
+        path = s.pop()
+        currentChild = path[-1]
  
-            if currentChild not in visited:
-                visited.append(currentChild)
+        if currentChild not in visited:
+            visited.append(currentChild)
 
-                parentsOfCurrentChild = []
+            parentsOfCurrentChild = []
 
                 # search for all parents for child
-                for parent in self.ancestors:
-                    if currentChild in self.ancestors[parent]:
-                        parentsOfCurrentChild.append(parent)
+            for parent in ancestors:
+                if currentChild in ancestors[parent]:
+                    parentsOfCurrentChild.append(parent)
 
                 # if he or she has no parents, we've hit the end of the path
                 
-                if len(parentsOfCurrentChild) == 0:
+            if len(parentsOfCurrentChild) == 0:
                     # store this path into allPossiblePaths
-                    allPossiblePaths.append(path)
-                else: 
+                allPossiblePaths.append(path)
+            else: 
                     # if he / she has parents, time to add those paths onto the stack
                     # for further traversing up the ladder
-                    for parent in parentsOfCurrentChild:
-                        newPath = list(path)
+                for parent in parentsOfCurrentChild:
+                    newPath = list(path)
                         # add the path of one OR both parents into stack
-                        newPath.append(parent)
-                        s.push(newPath)
+                    newPath.append(parent)
+                    s.push(newPath)
         
 
         # step 3) after compiling all possible paths using DFS, sort them to find the longest path
-        allPossiblePaths = sorted(allPossiblePaths)
-        print("All possible paths", allPossiblePaths)
-        longestPath = allPossiblePaths[0]
-        earliestAncestor = longestPath[-1]
+    allPossiblePaths = sorted(allPossiblePaths)
+    print("All possible paths", allPossiblePaths)
+    longestPath = allPossiblePaths[0]
+    earliestAncestor = longestPath[-1]
+    if earliestAncestor == starting_node:
+        return -1
+    else:
         return earliestAncestor
-
-s = SocialGraph()
 
 parentsChild = [  
   [1,3],
@@ -95,4 +93,6 @@ parentsChild = [
   [10,1]
 ]
 
-print(s.earliest_ancestor(parentsChild,6))
+g = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(parentsChild,6))
